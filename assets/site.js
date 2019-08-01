2160329f883cdc50d3479c57dae2f4d4ace0c1e6
Izu <rannick10@tutanota.com>
2019-08-01 02:22:14 +0200

    Add files

---
+++ assets/site.js
@@ -0,0 +1,18 @@
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    document.execCommand('copy');
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text);
+}
